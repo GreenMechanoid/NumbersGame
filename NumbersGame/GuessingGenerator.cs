6735858368b1db73964c010609caee7ab11fab20
Daniel Svensson <daniel.svensson682@gmail.com>
2022-09-13 14:56:10 +0200

    implemented standard game loop with fixed guesses and values. started implementing custom games with diffrent number of guesses and random values. made game loop if player wants to continue playing

---
+++ NumbersGame/GuessingGenerator.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+//Daniel Svensson .Net22
+namespace NumbersGame
+{
+
+    class Guesser
+    {
+        public void StandardGuessing(int numberOfGuesses, int randNumber, string Query)
+        {
+            Console.WriteLine("We got to Standard Guessing!");
+            int GuessCounter = 0;
+            int GuessedNumber;
+            bool CorrectGuess = false;
+            do
+            {
+                do
+                {
+                    Query = Console.ReadLine();
+                } while (int.TryParse(Query, out GuessedNumber)is false);
+
+                if (GuessedNumber == randNumber)
+                {
+                    Console.WriteLine("Grattis du gissade rätt!");
+                    CorrectGuess = true;
+                }
+                else
+                {
+                    Console.WriteLine("Tyvär du gissade fel!, försök igen");
+                }
+
+                GuessCounter++;
+            } while ((GuessCounter != numberOfGuesses) && CorrectGuess == false);
+            if (GuessCounter == numberOfGuesses)
+            {
+                Console.Clear();
+                Console.WriteLine("Tyvär så var det sista gissningen!, Det rätta nummret var "+ randNumber);
+            }
+        }
+
+        public void CustomGuessing(int numberOfGuesses)
+        {
+            Console.WriteLine("We got to Custom Guessing!");
+        }
+
+        public bool RestartGame(ConsoleKey inputVariable)
+        {
+            bool Restart = false;
+            Console.WriteLine("Vill du spela igen? Y/N");
+            do
+            {
+                Console.WriteLine("\nVänligen tryck antigen Y eller N..");
+                inputVariable = Console.ReadKey(false).Key;
+            } while (inputVariable != ConsoleKey.Y && inputVariable != ConsoleKey.N); // takes input from user to check Yes or no
+            Restart = true ? inputVariable == ConsoleKey.Y : inputVariable == ConsoleKey.N;
+            return Restart;
+        }
+
+    }
+
+
+
+
+
+}
--- NumbersGame/Program.cs
+++ NumbersGame/Program.cs
@@ -1,12 +1,70 @@
 ﻿using System;
-
+//Daniel Svensson .Net22
 namespace NumbersGame
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            bool NormalOrCustom = false;
+            bool GameState = true;
+            ConsoleKey YesOrNo;
+            Random rand = new Random();
+            Guesser engine = new Guesser();
+            int guesses = 5;
+            int Difficulty = 1;
+            string QueryString = ""; // get's string input from user for the guesses
+
+
+
+            do
+                switch (GameState)
+                {
+                    default: // normal operation
+                        int StandardRandNumber = rand.Next(1, 20);
+                        Console.WriteLine("Välkommen! Jag tänker på ett nummer.Kan du gissa vilket ?");
+
+                        do
+                        {
+                            Console.WriteLine("Vill du välja en svårighetsgrad eller vill du ha den normala? Y/N");
+                            YesOrNo = Console.ReadKey(false).Key;
+                        } while (YesOrNo != ConsoleKey.Y && YesOrNo != ConsoleKey.N); // takes input from user to check Yes or no
+
+                        NormalOrCustom = true ? YesOrNo == ConsoleKey.Y : YesOrNo == ConsoleKey.N; // do they want custom difficulty?
+                        Console.WriteLine();
+
+                        if (NormalOrCustom == true) // yes they do, let's make it a custom game
+                        {
+                            Console.WriteLine("Vänligen välj en svårighetsgrad mellan 1 och 5");
+                            QueryString = Console.ReadLine();
+                            while (int.TryParse(QueryString, out Difficulty) is false)
+                            {
+                                Console.WriteLine("Vänligen välj en svårighetsgrad mellan 1 och 5");
+                                QueryString = Console.ReadLine();
+                            }
+                            Difficulty = Convert.ToInt32(QueryString);
+                            engine.CustomGuessing(guesses);
+                        }
+                        else // normal guessing game with set difficulty, and no they dont
+                        {
+                            Console.WriteLine("Du kommer få 5 gissningar, med ett nummer mellan 1 och 20");
+                            engine.StandardGuessing(guesses, StandardRandNumber,QueryString);
+                        }
+
+                        GameState = engine.RestartGame(YesOrNo); // calls function to check if player wants to play again
+                        if (GameState == true)
+                        {
+                            Console.Clear(); // empty's the console for the new run
+                            break;
+                        }
+                        else { goto case false; }
+                    case false:
+                        Console.Clear();
+                        Console.WriteLine("\nTackar för att du spelade, Ha en trevlig dag!\n");
+                        break;
+                }
+            while (GameState == true);
+
         }
     }
 }
